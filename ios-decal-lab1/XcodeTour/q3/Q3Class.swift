fdaa94e99a4ca53125fe7b3d85ca7e30c7234c47
nguyenjessie <jessienguyen@berkeley.edu>
2018-02-08 15:25:52 -0800

    finish lab, check if wcontents here

---
+++ ios-decal-lab1/XcodeTour/q3/Q3Class.swift
@@ -0,0 +1,18 @@
+//
+//  Q3Class.swift
+//  XcodeTour
+//
+//  Created by Jessie Nguyen on 2/7/2018.
+//  Copyright © 2018 org.iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class Q3Class: UIViewController {
+
+    @IBOutlet weak var hello: UILabel!
+    
+    override func viewDidLoad() {
+        hello.text = "i am a view controller with a class set 😊"
+    }
+}
